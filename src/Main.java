136bd1b7cd635d87736c172daffbc2b8e549c268
Dany Mejía <168014765+Danymjia@users.noreply.github.com>
2025-01-08 21:09:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Login");
+        frame.setContentPane(new login().login);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+        frame.setPreferredSize(new Dimension(600,400));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
